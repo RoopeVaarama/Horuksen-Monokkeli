56cd820e886a65fa7f218d56233a2117d56a1906
joonas175 <36733885+joonas175@users.noreply.github.com>
2022-09-22 15:21:14 +0300

    Get server port from dotenv

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,8 +1,13 @@
+import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3001);
+  const configService = app.get(ConfigService);
+  const port = configService.get('SERVER_PORT');
+  await app.listen(port, () => {
+    console.info(`Listening on port ${port}`);
+  });
 }
 bootstrap();
