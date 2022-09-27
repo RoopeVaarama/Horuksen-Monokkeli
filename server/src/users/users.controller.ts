73b9bd7081528f11e014d90ac5825d4e64b59bf9
joonas175 <36733885+joonas175@users.noreply.github.com>
2022-09-27 17:57:49 +0300

    Fix string literals

--- server/src/users/users.controller.ts
+++ server/src/users/users.controller.ts
@@ -3,32 +3,32 @@ import { ApiCreatedResponse, ApiResponse, ApiTags } from '@nestjs/swagger';
 import { User } from './schemas/user.schema';
 import { UsersService } from './users.service';
 
-@ApiTags("users")
-@Controller("/users")
+@ApiTags('users')
+@Controller('/users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
-  @Get("/")
-  @ApiResponse({ status: 200, description: "All users returned", type: [User]})
+  @Get('/')
+  @ApiResponse({ status: 200, description: 'All users returned', type: [User]})
   async getUsers(): Promise<User[]> {
     return await this.usersService.getUsers();
   }
 
-  @Post("/")
-  @ApiCreatedResponse({ status: 200, description: "New user created", type: User })
+  @Post('/')
+  @ApiCreatedResponse({ status: 200, description: 'New user created', type: User })
   async createUser(@Body() user : User ): Promise<User> {
     return await this.usersService.createUser(user);
   }
 
-  @Patch("/:id")
-  @ApiResponse({ status: 200, description: "User updated", type: User})
-  async updateUser(@Param("id") id: String, @Body() user: User) {
+  @Patch('/:id')
+  @ApiResponse({ status: 200, description: 'User updated', type: User})
+  async updateUser(@Param('id') id: String, @Body() user: User) {
     return await this.usersService.updateUser(id, user);
   }
 
-  @Delete("/:id")
-  @ApiResponse({ status: 200, description: "User deleted", type: Boolean})
-  async deleteUser(@Param("id") id: String) {
+  @Delete('/:id')
+  @ApiResponse({ status: 200, description: 'User deleted', type: Boolean})
+  async deleteUser(@Param('id') id: String) {
     return await this.usersService.deleteUser(id);
   }
 }
