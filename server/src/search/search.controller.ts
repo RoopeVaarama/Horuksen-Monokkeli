9bdf1caf4e83863ef252d54c74e44939f4b366df
JuhanaKivela <kivelajuhana@gmail.com>
2022-10-10 15:16:34 +0300

    Add validy checking for every page of document

--- server/src/search/search.controller.ts
+++ server/src/search/search.controller.ts
@@ -17,8 +17,6 @@ export class SearchController {
   @Get('/parse/:file')
   async parse(@Param('file') file: string): Promise<PDFExtractResult> {
     this.checkFileValidity(file);
-    //TODO: maybe put this to variable and use that instead of
-    //extracting again in checkFileValidity
     return await this.parseService.parsePdf(`test_pdfs/${file}`);
   }
 
@@ -35,11 +33,14 @@ export class SearchController {
     const options = {};
     pdfExtract.extract(`test_pdfs/${file}`, options, (err, data) => {
       if (err) return console.log(err);
-      const page = data.pages[0];
-      const lines = PDFExtract.utils.pageToLines(page, 2);
-      const rows = PDFExtract.utils.extractTextRows(lines);
-      //console.log("Lines: ", lines, " Rows: ", rows);
-      if(rows == 0){
+      let linesInTotal = 0;
+      for(let pageIndex = 0; pageIndex < data.pages.length; ++pageIndex){
+        const page = data.pages[pageIndex];
+        const lines = PDFExtract.utils.pageToLines(page, 2);
+        linesInTotal+=lines;
+      }
+
+      if(linesInTotal == 0){
         //TODO: replace with something else
         console.log("This document seems to have no text. Are you sure you want to use this file?")
       }
