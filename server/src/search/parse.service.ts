54ef7e4fea8392807741cf336f3b6567a7cfa903
JuhanaKivela <kivelajuhana@gmail.com>
2022-10-11 00:24:54 +0300

    Move file checking to parse.service.ts

--- server/src/search/parse.service.ts
+++ server/src/search/parse.service.ts
@@ -4,9 +4,31 @@ import { PDFExtract, PDFExtractOptions, PDFExtractResult } from 'pdf.js-extract'
 @Injectable()
 export class ParseService {
 
-    async parsePdf(file: string):Promise<PDFExtractResult> {
+    async parsePdf(file: string): Promise<PDFExtractResult> {
+        return await this.checkFileValidity(file);
+    }
+
+    checkFileValidity(file: string) {
+        const PDFExtract = require('pdf.js-extract').PDFExtract;
         const pdfExtract = new PDFExtract();
-        const options: PDFExtractOptions = {};
-        return await pdfExtract.extract(file, options);
+        const options = {};
+        let extractedPDF = pdfExtract.extract(`${file}`, options, (err, data) => {
+            if (err){
+                console.log(err);
+                return null;
+            }
+            let linesInTotal = 0;
+            for (let pageIndex = 0; pageIndex < data.pages.length; ++pageIndex) {
+                const page = data.pages[pageIndex];
+                const lines = PDFExtract.utils.pageToLines(page, 2);
+                linesInTotal += lines;
+            }
+
+            if (linesInTotal == 0) {
+                //TODO: replace with something else
+                console.log("This document seems to have no text. Are you sure you want to use this file?")
+            }
+        });
+        return extractedPDF;
     }
 }
--- server/src/search/search.controller.ts
+++ server/src/search/search.controller.ts
@@ -2,7 +2,7 @@ import { Controller, Get, Post, Body, Param } from '@nestjs/common';
 import { ApiCreatedResponse, ApiResponse, ApiTags } from '@nestjs/swagger';
 import { ParseService } from './parse.service';
 import { SearchService } from './search.service';
-import { PDFExtractResult, PDFExtract } from 'pdf.js-extract';
+import { PDFExtractResult } from 'pdf.js-extract';
 import { Search } from './schemas/search.schema';
 import { Result } from './schemas/result.schema';
 
@@ -16,7 +16,6 @@ export class SearchController {
 
   @Get('/parse/:file')
   async parse(@Param('file') file: string): Promise<PDFExtractResult> {
-    this.checkFileValidity(file);
     return await this.parseService.parsePdf(`test_pdfs/${file}`);
   }
 
@@ -25,25 +24,5 @@ export class SearchController {
   async findValues(@Body() search: Search, @Param('file') file: string): Promise<Result[]> {
     const contents = await this.parseService.parsePdf(`test_pdfs/${file}`);
     return await this.service.search(contents, search);
-  }
-
-  private checkFileValidity(file) {
-    const PDFExtract = require('pdf.js-extract').PDFExtract;
-    const pdfExtract = new PDFExtract();
-    const options = {};
-    pdfExtract.extract(`test_pdfs/${file}`, options, (err, data) => {
-      if (err) return console.log(err);
-      let linesInTotal = 0;
-      for(let pageIndex = 0; pageIndex < data.pages.length; ++pageIndex){
-        const page = data.pages[pageIndex];
-        const lines = PDFExtract.utils.pageToLines(page, 2);
-        linesInTotal+=lines;
-      }
-
-      if(linesInTotal == 0){
-        //TODO: replace with something else
-        console.log("This document seems to have no text. Are you sure you want to use this file?")
-      }
-    });
   }
 }
