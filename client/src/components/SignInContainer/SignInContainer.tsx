5fb906cc25d9371f88b456c5a5d57e642e937cc0
Essi <essi.huhtanen@tuni.fi>
2022-10-14 10:59:15 +0300

    Changes to form, added change handler for inputs

--- client/src/components/SignInContainer/SignInContainer.tsx
+++ client/src/components/SignInContainer/SignInContainer.tsx
@@ -1,7 +1,9 @@
 import { styled, Typography, Button } from '@mui/material'
+import { TextField } from '@mui/material'
 import { Box } from '@mui/system'
 import { FormattedMessage } from 'react-intl'
 import SimpleToolbar from './SimpleToolbar'
+import { useState, ChangeEvent, FormEvent } from 'react'
 
 const TOOLBAR_HEIGHT = '60px'
 
@@ -13,33 +15,67 @@ const FormContainer = styled(Box)(({ theme }) => ({
   transform: 'translate(-50%, -50%)',
   padding: '25px',
   borderRadius: '5px',
-  border: 'solid'
+  border: 'solid',
+  alignItems: 'center'
 }))
 
 const SignInForm = styled('form')(({ theme }) => ({
   backgroundColor: theme.palette.background.paper,
   display: 'flex',
-  flexDirection: 'column'
+  flexDirection: 'column',
+  padding: theme.spacing(2, 3)
 }))
 
-const FormInput = styled('input')(({ theme }) => ({
-  marginTop: '5px',
-  marginBottom: '5px',
-  fontSize: '16px'
+const FormInput = styled(TextField)(({ theme }) => ({
+  marginTop: '10px',
+  marginBottom: '10px',
+  fontSize: '16px',
+  variant: 'outlined'
+}))
+
+const SignInButton = styled(Button)(({ theme }) => ({
+  margin: '10px',
+  variant: 'contained',
+  color: theme.palette.primary.main
 }))
 
 const SignInContainer = () => {
+  const [credentials, setCredentials] = useState({
+    email: '',
+    password: ''
+  })
+
+  const setEmail = (event: ChangeEvent<HTMLInputElement>): void => {
+    setCredentials((currentCred) => ({
+      ...currentCred,
+      email: event.target.value
+    }))
+  }
+
+  const setPassword = (event: ChangeEvent<HTMLInputElement>): void => {
+    setCredentials((currentCred) => ({
+      ...currentCred,
+      password: event.target.value
+    }))
+  }
+
+  const handleSubmit = (e: FormEvent<HTMLFormElement>): void => {
+    e.preventDefault()
+    console.log('Submitted')
+    // Varsinainen kirjautumisäksöni
+  }
+
   return (
     <div>
       <SimpleToolbar height={TOOLBAR_HEIGHT} />
       <FormContainer>
         <Typography variant='h6'>
-          <FormattedMessage id='signIn' defaultMessage='Kirjaudu sisään' />
+          <FormattedMessage id='signIn' defaultMessage='Kirjautuminen' />
         </Typography>
-        <SignInForm>
-          <FormInput type='email' name='email' placeholder='Sähköposti' />
-          <FormInput type='password' name='password' placeholder='Salasana' />
-          <Button variant='contained'>Kirjaudu sisään</Button>
+        <SignInForm onSubmit={handleSubmit}>
+          <FormInput label='Sähköposti' required onChange={setEmail} />
+          <FormInput label='Salasana' required onChange={setPassword} />
+          <SignInButton type='submit'>Kirjaudu sisään</SignInButton>
         </SignInForm>
       </FormContainer>
     </div>
