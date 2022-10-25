f3d7d4aefcaa78c02ffe8e3d7fcb117f4a4eb504
Volleyanvil <visual.sixtyfour@gmail.com>
2022-10-25 15:13:26 +0300

    File metadata schema, creation, get all

--- server/src/file/file.controller.ts
+++ server/src/file/file.controller.ts
@@ -2,6 +2,7 @@ import {
   Controller,
   Get,
   Post,
+  Delete,
   Body,
   Param,
   UploadedFile,
@@ -10,19 +11,22 @@ import {
   StreamableFile,
   Header,
   HttpException,
+  ParseFilePipe,
+  FileTypeValidator,
 } from '@nestjs/common';
 import { FileInterceptor } from '@nestjs/platform-express';
 import { ApiCreatedResponse, ApiConsumes, ApiResponse, ApiTags, ApiBody } from '@nestjs/swagger';
 import { FileService } from './file.service';
+import { FileMeta } from './schemas/filemeta.schema';
 
-@ApiTags('file')
-@Controller('/file')
+@ApiTags('files')
+@Controller('/files')
 export class FileController {
   constructor(private readonly service: FileService) {}
 
   @Post('/upload')
   @UseInterceptors(FileInterceptor('file'))
-  @ApiResponse({ status: 200, description: 'File successfully uploaded', type: Object })
+  @ApiResponse({ status: 201, description: 'File successfully uploaded', type: Object })
   @ApiConsumes('multipart/form-data')
   @ApiBody({
     schema: {
@@ -35,17 +39,32 @@ export class FileController {
       },
     },
   })
-  async uploadFile(@UploadedFile() file: Express.Multer.File) {
-    const response = {
-      originalname: file.originalname,
-      filename: file.filename,
-    };
-    return response;
+  async uploadFile(
+    @UploadedFile(
+      new ParseFilePipe({
+        validators: [new FileTypeValidator({ fileType: 'pdf' })],
+      }),
+    )
+    file: Express.Multer.File,
+  ): Promise<FileMeta> {
+    return await this.service.createFileMeta(file);
+  }
+
+  @Get('/')
+  @ApiResponse({ status: 200, description: 'All files returned', type: [FileMeta] })
+  async getFiles(): Promise<FileMeta[]> {
+    return await this.service.getFiles();
+  }
+
+  @Delete('/:id')
+  @ApiResponse({ status: 200, description: 'FileMeta deleted', type: Boolean })
+  async deleteFile(@Param('id') id: string) {
+    return await this.service.deleteFileMeta(id);
   }
 
   @Get('/get')
   @ApiResponse({ status: 200, description: 'All filenames returned', type: [String] })
-  async getFile(): Promise<string[]> {
+  async getFilenames(): Promise<string[]> {
     return await this.service.getAllFiles();
   }
 }
--- server/src/file/file.module.ts
+++ server/src/file/file.module.ts
@@ -1,10 +1,15 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { MulterModule } from '@nestjs/platform-express';
 import { FileController } from './file.controller';
 import { FileService } from './file.service';
-import { MulterModule } from '@nestjs/platform-express';
+import { FileMeta, FileMetaSchema } from './schemas/filemeta.schema';
 
 @Module({
-  imports: [MulterModule.register({ dest: './test_pdfs' })],
+  imports: [
+    MongooseModule.forFeature([{ name: FileMeta.name, schema: FileMetaSchema }]),
+    MulterModule.register({ dest: './test_pdfs' }),
+  ],
   providers: [FileService],
   controllers: [FileController],
   exports: [FileService],
