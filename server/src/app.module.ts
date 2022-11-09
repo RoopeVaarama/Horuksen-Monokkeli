b8640e1cad0cd92300d9366e8b28eccabccfa211
Roope.Vaarama <Roope.Vaarama@gmail.com>
2022-11-09 12:45:04 +0200

    user creation and authentication bases

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -3,34 +3,35 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { User, UserSchema } from './users/schemas/user.schema';
 import { SearchModule } from './search/search.module';
 import { FileModule } from './file/file.module';
-import { UsersModule } from './users/users.module';
 import { TemplateModule } from './template/template.module';
+import { AuthModule } from './auth/auth.module';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [
-    ConfigModule.forRoot({
-      envFilePath: ['.production.env', '.env'],
-    }),
-    MongooseModule.forRootAsync({
-      imports: [ConfigModule],
-      useFactory: async (configService: ConfigService) => {
-        const uri = configService.get<string>('MONGODB_URI');
-        console.info(`Using MongoDB URI ${uri}`);
-        return {
-          uri,
-        };
-      },
-      inject: [ConfigService],
-    }),
-    SearchModule,
-    UsersModule,
-    FileModule,
-    TemplateModule,
-  ],
-  controllers: [AppController],
-  providers: [AppService],
+    imports: [
+        ConfigModule.forRoot({
+            envFilePath: ['.production.env', '.env'],
+        }),
+        MongooseModule.forRootAsync({
+            imports: [ConfigModule],
+            useFactory: async (configService: ConfigService) => {
+                const uri = configService.get<string>('MONGODB_URI');
+                console.info(`Using MongoDB URI ${uri}`);
+                return {
+                    uri,
+                };
+            },
+            inject: [ConfigService],
+        }),
+        SearchModule,
+        FileModule,
+        TemplateModule,
+        AuthModule,
+        UserModule,
+    ],
+    controllers: [AppController],
+    providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
--- server/src/search/search.module.ts
+++ server/src/search/search.module.ts
@@ -8,12 +8,12 @@ import { Search, SearchSchema } from './schemas/search.schema';
 import { Template, TemplateSchema } from '../template/schemas/template.schema';
 
 @Module({
-  imports: [
-    MongooseModule.forFeature([{ name: Search.name, schema: SearchSchema }]),
-    MongooseModule.forFeature([{ name: Template.name, schema: TemplateSchema }]),
-  ],
-  providers: [SearchService, ParseService, TemplateService],
-  controllers: [SearchController],
-  exports: [SearchService, ParseService],
+    imports: [
+        MongooseModule.forFeature([{ name: Search.name, schema: SearchSchema }]),
+        MongooseModule.forFeature([{ name: Template.name, schema: TemplateSchema }]),
+    ],
+    providers: [SearchService, ParseService, TemplateService],
+    controllers: [SearchController],
+    exports: [SearchService, ParseService],
 })
-export class SearchModule {}
+export class SearchModule { }
