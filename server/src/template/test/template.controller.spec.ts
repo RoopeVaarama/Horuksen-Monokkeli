47026d49e3dd86a57ac870f4171de5a76e600980
Volleyanvil <visual.sixtyfour@gmail.com>
2022-11-15 18:14:28 +0200

    Added remaining controller tests, service mocking

--- server/src/template/test/template.controller.spec.ts
+++ server/src/template/test/template.controller.spec.ts
@@ -6,7 +6,6 @@ import { TemplateController } from '../template.controller';
 import { TemplateService } from '../template.service';
 import { Template, TemplateSchema } from '../schemas/template.schema';
 import { TemplateStub } from '../test/stubs/template.schema.stub';
-// import { Term, TermSchema } from '../schemas/term.schema';
 
 describe('TemplateController', () => {
   let templateController: TemplateController;
@@ -14,19 +13,42 @@ describe('TemplateController', () => {
   let mongod: MongoMemoryServer;
   let mongoConnection: Connection;
   let templateModel: Model<Template>;
-  // let termModel: Model<Term>;
 
   // Setup testing environment before running any tests
   beforeAll(async () => {
-    mongod = await MongoMemoryServer.create(); // Create Mocked MongoDB memory server instance
-    const uri = mongod.getUri(); // Get memory server instance URI
-    mongoConnection = (await connect(uri)).connection; // Connect to memory server
+    const MockTemplateService = {
+      provide: TemplateService,
+      useFactory: () => ({
+        createTemplate: jest.fn(() => {
+          return TemplateStub();
+        }),
+        getAllTemplates: jest.fn(() => {
+          return [TemplateStub()];
+        }),
+        getTemplatesByUserId: jest.fn(() => {
+          return [TemplateStub()];
+        }),
+        getTemplateById: jest.fn(() => {
+          return TemplateStub();
+        }),
+        updateTemplate: jest.fn(() => {
+          return TemplateStub();
+        }),
+        deleteTemplate: jest.fn(() => {
+          return true;
+        }),
+      }),
+    };
+
+    mongod = await MongoMemoryServer.create();
+    const uri = mongod.getUri();
+    mongoConnection = (await connect(uri)).connection;
     templateModel = mongoConnection.model(Template.name, TemplateSchema);
-    // termModel = mongoConnection.model(Term.name, TermSchema);
     const app: TestingModule = await Test.createTestingModule({
       controllers: [TemplateController],
       providers: [
         TemplateService,
+        MockTemplateService,
         { provide: getModelToken(Template.name), useValue: templateModel },
       ],
     }).compile();
@@ -51,22 +73,70 @@ describe('TemplateController', () => {
   });
 
   describe('Create Template', () => {
-    it('Should call create with & return saved object', async () => {
-      const spy = jest.spyOn(templateService, 'createTemplate'); // No need to mock when using actual service
+    it('Should be using mocked services for testing', async () => {
+      const spy = jest.spyOn(templateService, 'createTemplate');
+      const createdTemplate = await templateController.createTemplate(null);
+      expect(spy).toHaveBeenCalled();
+      expect(spy).toHaveBeenCalledWith(null);
+      expect(createdTemplate.title).toBe(TemplateStub().title); // Mocked should return stub given param null
+    });
+
+    it('Should call createTemplate & return modified created object', async () => {
+      const spy = jest.spyOn(templateService, 'createTemplate');
       const createdTemplate = await templateController.createTemplate(TemplateStub());
       expect(spy).toHaveBeenCalled();
       expect(spy).toHaveBeenCalledWith(TemplateStub());
       expect(createdTemplate.title).toBe(TemplateStub().title);
+      expect(createdTemplate.author).toBe(TemplateStub().author);
+      expect(createdTemplate.terms).toStrictEqual(TemplateStub().terms);
+      expect(createdTemplate).toHaveProperty('_id'); // Controller adds property _id  before returning
     });
 
-    it('Should return array of all documents', async () => {
+    it('Should call getAllTemplates & return array of documents', async () => {
       const spy = jest.spyOn(templateService, 'getAllTemplates');
-      await templateController.createTemplate(TemplateStub()); // Populate DB
       const getAll = await templateController.getTemplates();
       expect(spy).toHaveBeenCalled();
       expect(getAll).toHaveLength(1);
-      expect(getAll[0].title).toBe(TemplateStub().title);
-      expect(getAll[0]).toHaveProperty('_id');
+      expect(getAll[0]).toStrictEqual(TemplateStub());
+    });
+
+    it('Should call getTemplatesByUserId & return array of documents', async () => {
+      const someUid = 'some-uid';
+      const spy = jest.spyOn(templateService, 'getTemplatesByUserId');
+      const getByUid = await templateController.getTemplatesByUser(someUid);
+      expect(spy).toHaveBeenCalled();
+      expect(spy).toHaveBeenCalledWith(someUid);
+      expect(getByUid).toHaveLength(1);
+      expect(getByUid[0]).toStrictEqual(TemplateStub());
+    });
+
+    it('Should call getTemplateById & return found document', async () => {
+      // Note: Query related raised errors, like 404, are thrown and tested in services.
+      const someId = 'some-id';
+      const spy = jest.spyOn(templateService, 'getTemplateById');
+      const getById = await templateController.getTemplateById(someId);
+      expect(spy).toHaveBeenCalled();
+      expect(spy).toHaveBeenCalledWith(someId);
+      expect(getById).toStrictEqual(TemplateStub());
+    });
+
+    it('Should call updateTemplate & return updated document', async () => {
+      const someId = 'some-id';
+      const spy = jest.spyOn(templateService, 'updateTemplate');
+      const updatedTemplate = await templateController.updateTemplate(someId, TemplateStub());
+      expect(spy).toHaveBeenCalled();
+      expect(spy.mock.calls[0][0]).toBe(someId);
+      expect(spy.mock.calls[0][1]).toStrictEqual(TemplateStub());
+      expect(updatedTemplate).toStrictEqual(TemplateStub());
+    });
+
+    it('Should call deleteTemplate & return boolean', async () => {
+      const someId = 'some-id';
+      const spy = jest.spyOn(templateService, 'deleteTemplate');
+      const deletedTemplate = await templateController.deleteTemplate(someId);
+      expect(spy).toHaveBeenCalled();
+      expect(spy).toHaveBeenCalledWith(someId);
+      expect(deletedTemplate).toBeTruthy();
     });
   });
 });
