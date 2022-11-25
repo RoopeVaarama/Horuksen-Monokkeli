b1e9bc9374afc5d44cfa750a17170c7c4d2a5882
Volleyanvil <visual.sixtyfour@gmail.com>
2022-11-25 19:25:54 +0200

    User & auth unit test basic module def fix

--- server/src/auth/auth.service.spec.ts
+++ server/src/auth/auth.service.spec.ts
@@ -1,12 +1,21 @@
+import { getModelToken } from '@nestjs/mongoose';
 import { Test, TestingModule } from '@nestjs/testing';
+import { UserService } from '../user/user.service';
+import { User } from '../user/user.schema';
 import { AuthService } from './auth.service';
+import { JwtService } from '@nestjs/jwt';
 
 describe('AuthService', () => {
   let service: AuthService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [AuthService],
+      providers: [
+        AuthService,
+        JwtService,
+        UserService,
+        { provide: getModelToken(User.name), useValue: jest.fn() },
+      ],
     }).compile();
 
     service = module.get<AuthService>(AuthService);
