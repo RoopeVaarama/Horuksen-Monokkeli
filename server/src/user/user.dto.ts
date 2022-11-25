6e7f72bc3b5443529058eda55fd84d0c841ff42b
Volleyanvil <visual.sixtyfour@gmail.com>
2022-11-25 19:27:20 +0200

    Prettied up code to please eslint

--- server/src/user/user.dto.ts
+++ server/src/user/user.dto.ts
@@ -1,21 +1,21 @@
 export class CreateUserDto {
-    readonly username: string;
-    readonly firstName: string;
-    readonly lastName: string;
-    readonly email: string;
-    readonly password: string;
+  readonly username: string;
+  readonly firstName: string;
+  readonly lastName: string;
+  readonly email: string;
+  readonly password: string;
 }
 
 export class UpdateUserDto {
-    readonly id: string;
-    readonly username: string;
-    readonly firstName: string;
-    readonly lastName: string;
-    readonly email: string;
-    readonly password: string;
+  readonly id: string;
+  readonly username: string;
+  readonly firstName: string;
+  readonly lastName: string;
+  readonly email: string;
+  readonly password: string;
 }
 
 export class LoginDto {
-    readonly username: string;
-    readonly password: string;
+  readonly username: string;
+  readonly password: string;
 }
--- server/src/user/user.schema.ts
+++ server/src/user/user.schema.ts
@@ -1,31 +1,30 @@
-import { Prop, Schema, SchemaFactory } from "@nestjs/mongoose";
-import { ApiProperty } from "@nestjs/swagger";
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { ApiProperty } from '@nestjs/swagger';
 import * as mongoose from 'mongoose';
 
 export type UserDocument = User & mongoose.Document;
 
 @Schema({ timestamps: { createdAt: 'created_at' } })
 export class User {
-    @Prop({ unique: true, required: true, type: String })
-    @ApiProperty()
-    username: string;
+  @Prop({ unique: true, required: true, type: String })
+  @ApiProperty()
+  username: string;
 
-    @Prop({ type: String })
-    @ApiProperty()
-    firstName: string;
+  @Prop({ type: String })
+  @ApiProperty()
+  firstName: string;
 
-    @Prop({ type: String })
-    @ApiProperty()
-    lastName: string;
+  @Prop({ type: String })
+  @ApiProperty()
+  lastName: string;
 
-    @Prop({ type: String })
-    @ApiProperty()
-    email: string;
+  @Prop({ type: String })
+  @ApiProperty()
+  email: string;
 
-    @Prop({ type: String })
-    @ApiProperty()
-    password: string;
-
+  @Prop({ type: String })
+  @ApiProperty()
+  password: string;
 }
 
 export const UserSchema = SchemaFactory.createForClass(User);
