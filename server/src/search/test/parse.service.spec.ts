7eecec340b9375505189dda22f4475c367892b42
joonas175 <36733885+joonas175@users.noreply.github.com>
2022-12-05 15:06:31 +0200

    Add tests for ParseService

---
+++ server/src/search/test/parse.service.spec.ts
@@ -0,0 +1,48 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { getModelToken } from '@nestjs/mongoose';
+import { BadRequestException, HttpException, NotFoundException } from '@nestjs/common';
+import { ParseService } from '../parse.service';
+import { invoicePdfExpectedResult } from './json/invoice.pdf';
+
+describe('ParseService', () => {
+  let parseService: ParseService;
+
+  beforeAll(async () => {
+    const app: TestingModule = await Test.createTestingModule({
+      providers: [ParseService],
+    }).compile();
+
+    parseService = app.get<ParseService>(ParseService);
+  });
+
+  describe('Parsing document', () => {
+    it('Should return info of valid PDF file', async () => {
+      const invoicePath = `${__dirname}/../../../test_pdfs/invoice.pdf`;
+      const results = await parseService.parsePdf(invoicePath);
+
+      expect(results.meta.info).toEqual(invoicePdfExpectedResult.meta.info);
+      expect(results.pages).toEqual(invoicePdfExpectedResult.pages);
+    });
+
+    it('Should return HttpException on invalid PDF file', async () => {
+      const invoicePath = `${__dirname}/../../../test_pdfs/header.png`;
+      const promise = parseService.parsePdf(invoicePath);
+      await expect(promise).rejects.toThrow(HttpException);
+      await expect(promise).rejects.toThrow("it's not in PDF format");
+    });
+
+    it('Should return HttpException if file not found', async () => {
+      const invoicePath = `${__dirname}/../../../test_pdfs/fileNotFoundTest.pdf`;
+      const promise = parseService.parsePdf(invoicePath);
+      await expect(promise).rejects.toThrow(HttpException);
+      await expect(promise).rejects.toThrow('File not found.');
+    });
+
+    it('Should return HttpException if PDF has no text elements', async () => {
+      const invoicePath = `${__dirname}/../../../test_pdfs/images_1.pdf`;
+      const promise = parseService.parsePdf(invoicePath);
+      await expect(promise).rejects.toThrow(HttpException);
+      await expect(promise).rejects.toThrow('No content in PDF.');
+    });
+  });
+});
